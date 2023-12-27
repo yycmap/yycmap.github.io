08b7875f7b65e958afeb54c33a9b5075fc3157c6
yycmap <154763408+yycmap@users.noreply.github.com>
2023-12-27 13:02:37 -0700

    added search bar

--- index.js
+++ index.js
@@ -4,17 +4,20 @@
 let map;
 
 function initMap() {
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: { lat: 51.0447, lng: -114.0719 },
   });
   // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+  // Add geoJson as markers
   map.data.loadGeoJson(
     "SS_20231227.geojson",
   );
   let infowindow = new google.maps.InfoWindow();
+
+  //Click on a marker
   map.data.addListener('click', function (event) {
-    //alert(subjects.workclassmapped);
 
     //start api call
     fetch('https://data.calgary.ca/resource/c2es-76ed.json?permitnum=' + event.feature.getProperty('permitnumber')).then(function (response) {
@@ -64,8 +67,59 @@ function initMap() {
       console.warn('Something went wrong.', err);
     }); //end api
   });
+
+  //Search Bar
+  let searchMarkers = [];
+  const calgaryBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(50.8426, -114.0719), // Southwest corner of Calgary
+    new google.maps.LatLng(51.2080, -113.8116)  // Northeast corner of Calgary
+  );
+  const searchBox = new google.maps.places.SearchBox(document.getElementById('search-bar'), {
+    bounds: calgaryBounds,
+    componentRestrictions: { country: 'CA' } // Restrict to Canada
+  });
+  map.addListener('bounds_changed', function () { //honestly not sure what this does
+    searchBox.setBounds(map.getBounds());
+});
+  searchBox.addListener('places_changed', function () {
+    const places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+      // Add a marker to highlight the selected place
+      const marker = new google.maps.Marker({
+        map: map,
+        title: place.name,
+        position: place.geometry.location,
+        icon: {
+          url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', // URL to the blue pin image
+        }
+      });
+      searchMarkers.push(marker);
+    });
+
+
+    map.fitBounds(bounds);
+    map.setZoom(19);
+  });
+  map.addListener('click', function () {
+    searchMarkers.forEach(function (marker) {
+      marker.setMap(null);
+  });
+  searchMarkers = [];
+});
 }
 window.initMap = initMap;
+
 
 
 
